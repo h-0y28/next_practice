b60036e5384e1a74df59d4d8c392055c42958f1f
h-0y28 <s24015@gsm.hs.kr>
2025-02-21 16:33:06 +0900

    feat: add user list

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,59 @@
-export default function UserPage() {
-  return <h1>User Page</h1>;
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+async function getUsers(): Promise<User[]> {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!res.ok) {
+    throw new Error("user 정보 가져오기 실패");
+  }
+
+  return res.json();
+}
+
+export default async function UserPage() {
+  const users = await getUsers();
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>email: {user.email}</p>
+            <p>phone: {user.phone}</p>
+            <p>company: {user.company.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
